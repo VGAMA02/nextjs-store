35fc681cd1fb29c2e4dd22ec684816a84c29f168
Gama <gamagama90@hotmail.com>
2025-05-30 11:52:20 -0600

    metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { MainProducts } from "app/Components/home/MainProducts"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Future world",
+  description: "Welcome to the future wirld, an ecommerce from other century",
+  keywords: ["ecommerce","technology"]
+}
 
 export default function Home() {
   return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -2,10 +2,30 @@ import { ProductView } from "app/Components/product/ProductView"
 import { getProducts } from "app/services/shopify/products"
 import { redirect } from "next/navigation"
 
+
+
 interface ProductPageProps {
   searchParams: {
     id: string
   }
+}
+
+export async function generateMetada({searchParams}: ProductPageProps){
+  const id = searchParams.id;
+  const products = await getProducts(id)
+  const product = products[0]
+
+  return{
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph:{
+      images: [product.image]
+    }
+  }
+
+
+
 }
 
 export default async function ProductPage({ searchParams }: ProductPageProps) {
