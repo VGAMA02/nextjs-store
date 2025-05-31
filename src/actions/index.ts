5ef53850975a6a9426eda1c93d0efe4d874cc8a2
Gama <gamagama90@hotmail.com>
2025-05-31 12:49:57 -0600

    Graphql

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,40 @@
-"use server" //las funciones corren en el servidor
+"use server"
+import { GraphQLClientSingleton } from "app/graphql"
+import { createUserMutation } from "app/graphql/mutations/createUserMutation"
 
-export const  handleCreateUser = async (formData:any) => {
-  console.log("handleCreateUser", formData)
+interface CustomerCreateResponse {
+  customerCreate: {
+    customer: {
+      firstName: string
+      lastName: string
+      email: string
+      phone: string
+    } | null
+    customerUserErrors: {
+      field: string[]
+      message: string
+      code: string
+    }[]
+  }
+}
+
+export const handleCreateUser = async (formData: FormData) => {
+  const formDataObject = Object.fromEntries(formData)
+  delete formDataObject["password_confirmation"]
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const variables = {
+    input: {
+      ...formDataObject,
+      phone: '+52' + formDataObject.phone
+    }
+  }
+
+  const { customerCreate } = await graphqlClient.request<CustomerCreateResponse>(
+    createUserMutation,
+    variables
+  )
+
+  const { customerUserErrors, customer } = customerCreate
+  console.log(customer)
+  console.log(customerUserErrors)
 }
